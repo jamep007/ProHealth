075d1f950ff6fffaf114caa0880756cb9df54edf
James Reynolds <jamesreynolds3527@gmail.com>
2021-02-11 17:53:02 -0600

    Added more authentication logic. Working on login

--- lib/Auth/auth.dart
+++ lib/Auth/auth.dart
@@ -4,8 +4,12 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:senior_project_swauhealthapp/Services/storage.dart';
 
-Future<int> userSignUp(String name, String username, String password,
+final SecureStorage secureStorage = SecureStorage();
+
+//In progress
+Future<int> addNewUser(String name, String username, String password,
     String confirm, String phone, String email) async {
   int responseCode;
   if (password != confirm) {
@@ -14,9 +18,10 @@ Future<int> userSignUp(String name, String username, String password,
     return responseCode;
   } else {
     //Passwords match
-    var rand = new Random().nextInt(9999999999);
+    var rand = new Random().nextInt(99999999);
     String _userId = rand.toString();
 
+    //Send data to sql
     final response = await http.post("https://website-to-add-user.php", body: {
       "name": name,
       "user_id": _userId,
@@ -25,6 +30,7 @@ Future<int> userSignUp(String name, String username, String password,
       "phone": phone,
       "email": email,
     });
+
     if (response.statusCode == 200) {
       responseCode = 2; //Passwords match & successfully submitted
     } else {
@@ -32,6 +38,38 @@ Future<int> userSignUp(String name, String username, String password,
       responseCode = 3; //Passwords match but did NOT successfully submit
       throw Exception('Failed');
     }
+
+    //Store user info for login
+    if (responseCode == 3) {
+      secureStorage.writeSecureData('user_id', _userId);
+      secureStorage.writeSecureData('username', username);
+      secureStorage.writeSecureData('password', password);
+    }
+
     return responseCode;
   }
+}
+
+//In progress
+Future<int> userTryLogin(String username, String password) async {
+  int responseCode;
+  //Send data to sql
+  final response = await http.post("https://website-to-add-user.php", body: {
+    "username": username,
+    "password": password,
+  });
+
+  if (response.statusCode == 200) {
+    print(response.statusCode);
+  } else {
+    print(response.statusCode);
+    throw Exception('Failed');
+  }
+
+  //Store user info for login
+  //secureStorage.writeSecureData('user_id', _userId);
+  secureStorage.writeSecureData('username', username);
+  secureStorage.writeSecureData('password', password);
+
+  return responseCode;
 }
--- lib/Auth/signup.dart
+++ lib/Auth/signup.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -11,12 +12,23 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  var _formKey = GlobalKey<FormState>();
+
   TextEditingController name = new TextEditingController();
+  static final validNameCharacters = RegExp(r'^[a-zA-Z]');
+
   TextEditingController username = new TextEditingController();
+  static final validUsernameCharacters = RegExp(r'^[a-zA-Z0-9]');
+
   TextEditingController password = new TextEditingController();
   TextEditingController confirm = new TextEditingController();
+  static final validPassCharacters = RegExp(r'^[a-zA-Z0-9_\-=@,\.;]+$');
+
   TextEditingController phone = new TextEditingController();
+  static final validPhoneCharacters = RegExp(r'^[0-9]');
+
   TextEditingController email = new TextEditingController();
+  static final validEmailCharacters = RegExp(r'^[a-zA-Z0-9_\@\.]');
 
   @override
   Widget build(BuildContext context) {
@@ -37,112 +49,239 @@ class _SignUpPageState extends State<SignUpPage> {
                   child: Text("Landscape"),
                 )
               : SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        top: 40, left: 20.0, right: 20.0, bottom: 40),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        TextField(
-                          controller: name,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Full Name",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          controller: username,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Username",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Divider(),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          controller: password,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Password",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          controller: confirm,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Confirm Password",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Divider(),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          controller: phone,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Phone",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          controller: email,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Email",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        FlatButton(
-                          color: Colors.blue,
-                          child: Text(
+                  child: Form(
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 40, left: 20.0, right: 20.0, bottom: 40),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
                             "Sign Up",
-                            style: TextStyle(fontSize: 15, color: Colors.white),
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          onPressed: () {
-                            userSignUp(name.text, username.text, password.text,
-                                confirm.text, phone.text, email.text);
-                            Navigator.of(context).popAndPushNamed("/profile");
-                          },
-                        ),
-                      ],
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Text(
+                            "Fields with '*' are required.",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: name,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "Please enter your name.";
+                              } else if (!validNameCharacters.hasMatch(value)) {
+                                return "Remove any non-alphabetic characters!";
+                              } else if (value.length > 20) {
+                                return "Your name is too long! Max length is 20 characters.";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Full Name *",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: username,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "Please enter your Username.";
+                              } else if (!validUsernameCharacters
+                                  .hasMatch(value)) {
+                                return "Remove any special characters!";
+                              } else if (value.length > 12) {
+                                return "Your Username is too long. Max length is 12 characters.";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Username *",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Divider(),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: password,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "Please enter your password.";
+                              } else if (!validPassCharacters.hasMatch(value)) {
+                                return "Your password has unaccepted symbols.";
+                              } else if (value.length > 20) {
+                                return "Your password is too long! Max length is 100 characters.";
+                              }
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Password *",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: confirm,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return "Please enter your password.";
+                              } else if (!validPassCharacters.hasMatch(value)) {
+                                return "Your password has unaccepted symbols.";
+                              } else if (value != password.text) {
+                                return "Your passwords do not match.";
+                              }
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Confirm Password *",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Divider(),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: phone,
+                            validator: (String value) {
+                              if (!validPhoneCharacters.hasMatch(value) &&
+                                  !value.isEmpty) {
+                                return "Your phone number must only contain numbers!";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Phone",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            controller: email,
+                            validator: (String value) {
+                              if (!validEmailCharacters.hasMatch(value) &&
+                                  !value.isEmpty) {
+                                return "Your email has unaccepted symbols.";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Email",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          FlatButton(
+                            color: Colors.blue,
+                            child: Text(
+                              "Sign Up",
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                if (_formKey.currentState.validate()) {
+                                  _userSignUp(context);
+                                }
+                              });
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
         },
       ),
     );
+  }
+
+  void _userSignUp(BuildContext context) async {
+    final int responseCode = await addNewUser(name.text, username.text,
+        password.text, confirm.text, phone.text, email.text);
+    print(responseCode);
+    switch (responseCode) {
+      case 1:
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: Text("Passwords do NOT match!"),
+                  content: Text("Please re-enter the passwords so they match."),
+                  actions: [
+                    FlatButton(
+                      child: Text("Ok"),
+                      onPressed: () {},
+                    ),
+                  ],
+                  elevation: 20,
+                ),
+            barrierDismissible: true);
+        break;
+      case 2:
+        Navigator.of(context).popAndPushNamed("/profile");
+        break;
+      case 3:
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: Text("Error"),
+                  content: Text(
+                      "There was an error submitting the sign up. Please come back later."),
+                  actions: [
+                    FlatButton(
+                      child: Text("Ok"),
+                      onPressed: () {},
+                    ),
+                  ],
+                  elevation: 20,
+                ),
+            barrierDismissible: true);
+        break;
+      default:
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: Text("Error"),
+                  content: Text(
+                      "There is some unknown error. Response code: $responseCode"),
+                  actions: [
+                    FlatButton(
+                      child: Text("Ok"),
+                      onPressed: () => exit(0),
+                    ),
+                  ],
+                  elevation: 20,
+                ),
+            barrierDismissible: false);
+        break;
+    }
   }
 }
